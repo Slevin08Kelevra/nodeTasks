7ee7934156d5fccd7c08e599cef21716ffcc7b57
pablo <pablopaparini@hotmail.com>
2021-04-30 19:36:49 +0200

    unlock doned

--- automation/commands/work/basics.js
+++ automation/commands/work/basics.js
@@ -1,44 +1,37 @@
 let basics = {
 
     'release.my.information': async (wsConns) => {
-        let ret = ['not waiting something is wrong']
-        if (!wsConns.get("BI_COMPUTER")) {
-            ret = ['web socket not connected!']
-        } else {
-            let { ws, obs } = wsConns.get("BI_COMPUTER")
-            ws.send('showMyInf')
-            let message = ""
-            try {
-                message = await obs.expect()
-            } catch (error) {
-                message = error
-            }
-    
-            ret = [message]
-        }
-        
-        return ret
+
+        return await execute(wsConns, 'showMyInf')
+
     },
     'unlock': async (wsConns) => {
-        let ret = ['not waiting something is wrong']
-        if (!wsConns.get("BI_COMPUTER")) {
-            ret = ['web socket not connected!']
-        } else {
-            let { ws, obs } = wsConns.get("BI_COMPUTER")
-            ws.send('unlock')
-            let message = ""
-            try {
-                message = await obs.expect()
-            } catch (error) {
-                message = error
-            }
-    
-            ret = [message]
-        }
-        
-        return ret
+
+        return await execute(wsConns, 'unlock')
+f
     }
 
 }
+
+async function execute(wsConns, doWhat) {
+    let ret = ['not waiting something is wrong']
+    if (!wsConns.get("BI_COMPUTER")) {
+        ret = ['web socket not connected!']
+    } else {
+        let { ws, obs } = wsConns.get("BI_COMPUTER")
+        ws.send(doWhat)
+        let message = ""
+        try {
+            message = await obs.expect()
+        } catch (error) {
+            message = error
+        }
+
+        ret = [message]
+    }
+
+    return ret
+}
+
 
 module.exports = basics;
