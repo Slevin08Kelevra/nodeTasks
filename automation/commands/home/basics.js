e8a54782029d818448294bc48952481dd14009c3
pablo <pablopaparini@hotmail.com>
2021-04-30 15:29:04 +0200

    unlock

---
+++ automation/commands/home/basics.js
@@ -0,0 +1,15 @@
+
+let home = {
+
+    'unlock.the.gate': async ()=> {
+        return ['gate unlocked']
+    },
+    'ventilation.start': async ()=>{
+        return ['ventilation start']
+    },
+    'smelly.cat.start': async ()=>{
+        return ['ventilation start']
+    }
+}
+
+module.exports = home;
---
+++ automation/unlock.js
@@ -0,0 +1,31 @@
+const SerialPort = require('serialport')
+var myArgs = process.argv.slice(2);
+
+let path = ''
+let ArduinoPort = ''
+SerialPort.list().then(ports => {
+    let done = false
+    let count = 0
+    let allports = ports.length
+    ports.forEach(function (port) {
+        count = count + 1
+        pm = port.manufacturer
+
+        if (typeof pm !== 'undefined' && pm.includes('arduino')) {
+            path = port.path
+            ArduinoPort = new SerialPort(path, { baudRate: 9600 })
+            ArduinoPort.on('open', function () {
+                ArduinoPort.write(myArgs[0], function (err, result) {
+                    if (err) {
+                        console.log('ERROR: ' + err);
+                    }
+                    console.log('OK: arduino cmd received')
+                });
+            })
+            done = true
+        }
+        if (count === allports && done === false) {
+            console.log(`ERROR: can't find any arduino`)
+        }
+    })
+})
