a8e49433bb6f9f8c9dd1d47656d3ce2f6cc339e0
pablo <pablopaparini@hotmail.com>
2021-05-08 18:06:12 +0200

    faltantes para zip

--- automation/backSensibleFiles.js
+++ automation/backSensibleFiles.js
@@ -6,13 +6,21 @@ if (fs.existsSync("/home/pablo/Documents/repos/sensibleBack/back.zip")) {
     fs.unlinkSync("/home/pablo/Documents/repos/sensibleBack/back.zip");
 }
 
+if (fs.existsSync("/home/pablo/Documents/repos/sensibleBack/back.kript.zip")) {
+    fs.unlinkSync("/home/pablo/Documents/repos/sensibleBack/back.kript.zip");
+}
+
+
 
 zip.addFile("/home/pablo/Documents/repos/nodeTasks/automation/reqValidator.js");
 zip.addFile("/home/pablo/Documents/repos/speech/app/src/main/java/com/pcordoba/speechtotexttest1/SignUtil.java");
 zip.addFile("/home/pablo/Documents/repos/nodeTasks/automation/Automation.postman_collection.json");
+//zip.addFolder("path/to/folder");
 
 zip.archive("/home/pablo/Documents/repos/sensibleBack/back.zip").then(function () {
     console.log("done");
 }, function (err) {
     console.log(err);
 });
+
+//zip --encrypt back.kript.zip back.zip
