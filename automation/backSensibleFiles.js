44052b1c8e7495e7cd25c774bd717cf323e34add
Your Name <pablo@paparini>
2021-07-17 12:49:50 +0200

    baks strategy

--- automation/backSensibleFiles.js
+++ automation/backSensibleFiles.js
@@ -1,6 +1,7 @@
 const zl = require("zip-lib");
 const zip = new zl.Zip();
 const fs = require('fs');
+const spawn = require('child_process').spawn;
 
 if (fs.existsSync("/home/pablo/Documents/repos/sensibleBack/back.zip")) {
     fs.unlinkSync("/home/pablo/Documents/repos/sensibleBack/back.zip");
@@ -13,15 +14,22 @@ if (fs.existsSync("/home/pablo/Documents/repos/sensibleBack/back.kript.zip")) {
 
 
 zip.addFile("/home/pablo/Documents/repos/nodeTasks/automation/reqValidator.js");
+zip.addFile("/home/pablo/Documents/repos/nodeTasks/automation/certDoc.txt");
 zip.addFile("/home/pablo/Documents/repos/speech/app/src/main/java/com/pcordoba/speechtotexttest1/SignUtil.java");
 zip.addFile("/home/pablo/Documents/repos/nodeTasks/automation/Automation.postman_collection.json");
 //zip.addFolder("path/to/folder");
 
 zip.archive("/home/pablo/Documents/repos/sensibleBack/back.zip").then(function () {
-    fs.copyFile('/home/pablo/Documents/repos/sensibleBack/back.zip', '/home/pablo/Dropbox/tasVids/back.zip', (err) => {
-        if (err) throw err;
-        console.log("done");
-      });
+    let zip = spawn('zip', ['--password', '1234qwer', '/home/pablo/Documents/repos/sensibleBack/back.kript.zip', '/home/pablo/Documents/repos/sensibleBack/back.zip']);
+
+    zip.on('exit', function (code) {
+        fs.copyFile('/home/pablo/Documents/repos/sensibleBack/back.kript.zip', '/home/pablo/Dropbox/tasVids/back.kript.zip', (err) => {
+            if (err) throw err;
+            console.log("done");
+        });
+    });
+
+
 }, function (err) {
     console.log(err);
 });
