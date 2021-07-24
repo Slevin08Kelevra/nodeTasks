f35bbce9d17ff4e8448036c4944022e711f85e4c
Your Name <pablo@paparini>
2021-07-24 11:29:18 +0200

    adding autostart on git pull

--- automation/startWsClient.js
+++ automation/startWsClient.js
@@ -1,5 +1,31 @@
 const wsClient = require('./wsClient');
 const gralUtils = require('./gralUtils')
+const simpleGit = require('simple-git');
+const props = require('./../props')
+
+
+let git = simpleGit(props.schedules.git_prop_folder);
+git.pull((err, update) => {
+    if(update && update.summary.changes) {
+        setTimeout(() => {
+            const time = new Date();
+            const filename = "restart.do"
+  
+            try {
+              fs.utimesSync(filename, time, time);
+            } catch (err) {
+              fs.closeSync(fs.openSync(filename, 'w'));
+            }
+          }, 10000);
+          gralUtils.logInfo('Git pull changes, restarting in 10 secs')
+    } else {
+        gralUtils.logInfo('Git with no changes, keep as if')
+    }
+
+    if (err){
+        gralUtils.logError('Cant pull from git')
+    }
+  })
 
 gralUtils.getGitProps((localhost, remotehost, status)=>{
     if (status == "wifi"){
