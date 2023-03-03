6e4d6bf5e019f6ef65b8ec0b59f17c81a0da1d4c
PPAPARIN <ppaparin@ford.com>
2023-03-03 20:11:56 +0100

    cambios en la lib de node para unlock

--- automation/unlock.js
+++ automation/unlock.js
@@ -1,4 +1,4 @@
-const SerialPort = require('serialport')
+const { SerialPort } = require('serialport')
 var myArgs = process.argv.slice(2);
 
 let path = ''
@@ -11,9 +11,9 @@ SerialPort.list().then(ports => {
         count = count + 1
         pm = port.manufacturer
 
-        if (typeof pm !== 'undefined' && pm.includes('arduino')) {
+        if (typeof pm !== 'undefined' && pm.includes('Microsoft')) {
             path = port.path
-            ArduinoPort = new SerialPort(path, { baudRate: 9600 })
+            ArduinoPort = new SerialPort({ path: path, baudRate: 9600 })
             ArduinoPort.on('open', function () {
                 ArduinoPort.write(myArgs[0], function (err, result) {
                     if (err) {
